9bf2ccf7a73da27a1a05744b989bfa5c7c788bc0
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-03-12 03:02:33 +0300

    fix: min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+exports.min = function min(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    const sortedArr = array;
+    sortedArr.sort((a, b) => a - b);
+    return sortedArr[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    const sortedArr = array;
+    sortedArr.sort((a, b) => b - a);
+    return sortedArr[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = []) {
+    if (array.length === 0) {
+        return 0;
+    }
+    const itemCount = array.length;
+    let sum = 0;
+    array.forEach((item) => {
+        sum += item;
+    });
+    return sum / itemCount;
+};
